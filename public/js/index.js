0cfa489afaa3bfd89c9b4cbf8846e2dcfb7e4967
Krishna Chaitanya <bkchaitan94@gmail.com>
2013-07-14 10:10:33 +0530

    Index create room button and popular rooms script

--- public/js/index.js
+++ public/js/index.js
@@ -1,74 +1,3 @@
-$(document).ready(
-    function(){
-      $('#for-button').click(function(){
-          $.ajax({
-           type: "POST",
-           url: '/submit',
-           data: {'message':$('#for-text').val() , 'side':'for' , 'session':sess,'time':new Date().getTime()}, // serializes the form's elements.
-           success: function(data)
-           {$('#for-text').val("");
-           }
-         });
-          return false;
-
-  });
-
-      $('#against-button').click(function(){
-          $.ajax({
-           type: "POST",
-           url: '/submit',
-           data: {'message':$('#against-text').val() , 'side':'against' , 'session':sess,'time':new Date().getTime()}, // serializes the form's elements.
-           success: function(data)
-           {
-            $('#against-text').val("");
-           }
-         });
-          return false;
-  });
-
-setInterval(forcheck,1000);
-setInterval(againstcheck,1000);
-
-
-function forcheck(){
-  if($('.lposts div:last-child()').attr('data-id') == undefined ) lid = 0;
-  else lid = $('.lposts div:last-child()').attr('data-id');
-  $.ajax({
-  url: '/for',
-  data: {'id':lid,'session':sess},
-  success: function(data){console.log(data);
-    data = JSON.parse(data);
-  for(i=0;i<data.length;i++){
-      addLpost(data[i]);
-    }
-  }
-});
-
-
-}
-
-
-function againstcheck(){
-  if($('.rposts div:last-child()').attr('data-id') == undefined ) rid = 0;
-  else rid = $('.rposts div:last-child()').attr('data-id');
-  
-  $.ajax({
-  url: '/against',
-  data: {'id':rid,'session':sess},
-  success: function(data){console.log(data);
-    data = JSON.parse(data);
-    for(i=0;i<data.length;i++){
-      addRpost(data[i]);
-    }
-
-  }
-});
-
-
-}
-
-});
-
 $(document).ready(function(){
   $.yql('select * from boss.search where q="" and sites=#{url} and ck="dj0yJmk9YWF3ODdGNWZPYjg2JmQ9WVdrOWVsWlZNRk5KTldFbWNHbzlNVEEyTURFNU1qWXkmcz1jb25zdW1lcnNlY3JldCZ4PTUz" and secret="a3d93853ba3bad8a99a175e8ffa90a702cd08cfa"',{url:'http://www.ptinews.com'},function(data){
     console.log(data.query.results.bossresponse.web.results.result);
@@ -80,4 +9,12 @@ $(document).ready(function(){
     r += "</ul>";
     $(".news-cont").html(r);
   });
+
+  $("#createRoom").click(function(){
+  	window.open("room.php/"+$("#room").val(),"_self");
+  });
+
+  $(".rooms-cont ul li").click(function(){
+  	window.open("room.php/"+$(this).text(),"_self");
+  })
 });
